093eb77bb82c78306947dc5c0e7091e56bd62c7f
Weber-droid <emmanueltotimeh2002@gmail.com>
2025-01-31 05:53:35 +0000

    Fix/Bug

--- src/main/java/com/nuelle/to_do/list/repository/UserRepository.java
+++ src/main/java/com/nuelle/to_do/list/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.nuelle.to_do.list.repository;
 
 import com.nuelle.to_do.list.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+//import java.lang.ScopedValue;
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional <User> findByUsername(String username);
 }
---
+++ src/main/java/com/nuelle/to_do/list/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.nuelle.to_do.list.service;
+
+
+import com.nuelle.to_do.list.model.User;
+import com.nuelle.to_do.list.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import java.util.List;
+//import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+}
